2bc4bb416237af0e081f1a4891db60275151c758
ridmal <ridmal19@gmail.com>
2017-11-10 22:26:00 +0530

    chnages in noe

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
+app.get('/', (req, res) => {
 
+      res.status(200).send("ridmal");
+    
+});
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
  
